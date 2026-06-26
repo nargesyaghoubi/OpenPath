f5720f307c09ca8bc0612d4d291c3b5cf943c0e7
Narges Yaghoubi <146190598+nargesyaghoubi@users.noreply.github.com>
2026-06-26 22:37:12 +0330

    update types with Category and OpportunityType

--- types/index.ts
+++ types/index.ts
@@ -1,20 +1,5 @@
-export interface Opportunity {
-    id: string
-    title: string
-    organization: string
-    category: string
-    location: string
-    type: string
-    deadline: string
-    description: string
-    requirements: string[]
-    applyLink: string
-    tags: string[]
-    featured?: boolean   // features is optional 
-    createdAt?: string   // createdAt is optional 
-  }
-  
-  export type Category =
+
+export type Category =
     | "Job"
     | "Internship"
     | "Scholarship"
@@ -22,12 +7,28 @@ export interface Opportunity {
     | "Remote Work"
     | "Training Program"
     | "Volunteer Work"
-  
-  export type OpportunityType = "Remote" | "On-site" | "Hybrid"
-  
-  export interface User {
+
+export type OpportunityType = "Remote" | "On-site" | "Hybrid"
+
+export interface Opportunity {
+    id: string
+    title: string
+    organization: string
+    category: Category
+    location: string
+    type: OpportunityType
+    deadline: string
+    description: string
+    requirements: string[]
+    applyLink: string
+    tags: string[]
+    featured?: boolean   // optional
+    createdAt?: string   // optional
+}
+
+export interface User {
     id: string
     name: string
     email: string
     role: "user" | "admin"
-  }
+}
